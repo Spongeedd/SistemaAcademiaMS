3f268b884dd4e997b3ee03d6e8f55f9e7d70e19d
aluno <aluno@decom>
2022-10-17 09:29:26 -0300

    Revert "Adicionado base para interface Cadastrar"
    This reverts commit 849b3597c1a7589f82c891d1f60fc6bd06cdc5ca.

--- academia/src/main/java/com/academia/App.java
+++ academia/src/main/java/com/academia/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("InterfaceLogin"), 640, 385);
+        scene = new Scene(loadFXML("InterfaceLogin"), 640, 384);
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
