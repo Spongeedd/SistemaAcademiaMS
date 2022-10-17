5f773f236f20233d84a321f63d3116b01beee9c0
odaviml <davimartinslage2005@gmail.com>
2022-10-17 09:10:45 -0300

    Criado base interface funcionários #3

--- academia/src/main/java/com/academia/Controller.java
+++ academia/src/main/java/com/academia/Controller.java
@@ -2,11 +2,39 @@ package com.academia;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller {
 
     @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+    private TextField senhaInput;
+
+    @FXML
+    private TextField funcionarioInput;
+
+    @FXML
+    private Button btnEntrar;
+    
+    @FXML
+    private void exibeInfo() throws IOException {
+        String senha = senhaInput.getText();
+        String login = funcionarioInput.getText();
+
+        System.out.println(login + " " + senha);
+        
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(App.class.getResource("InterfaceCadastrar.fxml"));
+        stage.setScene(new Scene(root));
+        stage.setTitle("Cadastrar");
+        stage.setResizable(false);
+        stage.show();
+        stage = (Stage) btnEntrar.getScene().getWindow();
+        stage.close();
     }
+
 }
