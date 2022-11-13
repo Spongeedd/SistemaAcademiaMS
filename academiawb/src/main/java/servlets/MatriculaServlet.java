e75beccab27dbd951ddde2a5500fec278d2abb25
Iaas0912 <lobinhaaacraft@gmail.com>
2022-11-13 13:26:37 -0300

    #20 Interface web matricula

--- academiawb/src/main/java/servlets/MatriculaServlet.java
+++ academiawb/src/main/java/servlets/MatriculaServlet.java
@@ -1,45 +1,83 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
-package com.sistemaacademia.academiaweb.servlets;
+package servlets;
 
+import academiawb.model.service.MatriculaService;
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
+import java.sql.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author Aluno
- */
 @WebServlet(name = "MatriculaServlet", urlPatterns = {"/MatriculaServlet"})
 public class MatriculaServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        
-        String nome = request.getParameter("nome");
-        String cpf = request.getParameter("cpf");
-        String data = request.getParameter("data");
-        String email = request.getParameter("email");
-        String endereco = request.getParameter("endereco");
-        String telefone = request.getParameter("telefone");
-        String plano = request.getParameter("plano");
-        String pacote = request.getParameter("pacote");
+            throws ServletException, IOException, SQLException, ClassNotFoundException {
+            String operacao = request.getParameter("op");
+            
+            String nome;
+            String cpf;
+            String datastr;
+            String email;
+            String endereco;
+            String telefone;
+
+            String plano;
+            String pacote;
+            Date nascimento;
+            
+            String idstr;
+            Integer id;
+            
+            
+            
+            switch (operacao) {
+                case "a":
+                    nome = request.getParameter("nome");
+                    cpf = request.getParameter("cpf");
+                    datastr = request.getParameter("nascimento");
+                    email = request.getParameter("email");
+                    endereco = request.getParameter("endereco");
+                    telefone = request.getParameter("telefone");
+
+                    plano = request.getParameter("plano");
+                    pacote = request.getParameter("pacote");
+                    nascimento = Date.valueOf(datastr);
+
+                    MatriculaService.adicionarMatricula(nome, cpf, nascimento, endereco, telefone, email, plano, pacote);
+                    response.sendRedirect("Funcionario.jsp");
+                    break;
+                case "d":
+                    idstr = request.getParameter("id");
+                    id = Integer.valueOf(idstr);
+                    MatriculaService.removerMatricula(id);
+                    response.sendRedirect("Funcionario.jsp");
+                    break;
+                case "u":
+                    idstr = request.getParameter("id");
+                    id = Integer.parseInt(idstr);
+                    
+                    nome = request.getParameter("nome");
+                    cpf = request.getParameter("cpf");
+                    datastr = request.getParameter("nascimento");
+                    email = request.getParameter("email");
+                    endereco = request.getParameter("endereco");
+                    telefone = request.getParameter("telefone");
+                    plano = request.getParameter("plano");
+                    pacote = request.getParameter("pacote");
+                    nascimento = Date.valueOf(datastr);
+                    
+                    MatriculaService.editarMatricula(id, nome, cpf, nascimento, endereco, telefone, email, plano, pacote);
+                    response.sendRedirect("Funcionario.jsp");
+                    break;
+                default:
+                    response.sendRedirect("Matricula.jsp");
+                    break;
+    }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -54,7 +92,13 @@ public class MatriculaServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(MatriculaServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MatriculaServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -68,7 +112,13 @@ public class MatriculaServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(MatriculaServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MatriculaServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
